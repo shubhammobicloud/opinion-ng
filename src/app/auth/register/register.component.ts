5ccfa93a692dc5dda3233df29f5e19b05ef51efd
Shubham Kalamkar <shubham.mobicloud@gmail.com>
2024-02-14 18:57:47 +0530

    form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm!:FormGroup
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name:[""],
+      email:["", Validators.email],
+      password:["", Validators.required]
+    })
+  }
 
+  onSubmit(){
+
+  }
 }
---
+++ src/constants/api-endpoints.ts
@@ -0,0 +1,3 @@
+export const USER = {
+  CREATE:"user/create"
+}
