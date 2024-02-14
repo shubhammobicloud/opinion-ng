572752369ac3cd76510aaf82a285c9a06d19c3d8
Shubham Kalamkar <shubham.mobicloud@gmail.com>
2024-02-14 18:36:35 +0530

    routing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {path:'auth',loadChildren:()=>import('./auth/auth.module').then(m=>m.AuthModule)}
+];
